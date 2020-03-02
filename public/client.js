1855944139204f6288448b938d6e3120f48938b8
Abdullah Yusefi <a.yusefi1991@gmail.com>
2020-03-02 15:00:47 +0300

    Eth0 Added!

--- public/client.js
+++ public/client.js
@@ -1,48 +1,66 @@
 
-const button = document.getElementById('myButton');
-const buttonkayit = document.getElementById('kayit')
-const messageOne = document.querySelector('#message-1')
-const messageTwo = document.querySelector("#message-2")
+const eth0Form = document.querySelector('#eth0form');
+const eth0IP = document.querySelector('#eth0-ip-address')
+const eth0Netmask = document.querySelector('#eth0-net-mask')
+const eth0Gateway = document.querySelector('#eth0-gate-way')
+const eth0DNS = document.querySelector("#eth0-dns-server")
 const messageThree = document.querySelector('#message-3')
 const messageFour = document.querySelector('#message-4')
 const messageFive = document.querySelector('#message-5')
 const messageSave = document.querySelector('#message-save')
+const messageEth0Save = document.querySelector('#message-eth0save')
 
-button.addEventListener('click', function(e) {
+// button.addEventListener('click', function(e) {
+//     e.preventDefault()
+
+//     messageOne.textContent = 'Loading...'
+//     messageTwo.textContent = ''
+//     messageThree.textContent = ''
+//     messageFour.textContent = ''
+//     messageFive.textContent = ''
+
+//     fetch('/interface?int=wlo1').then((response) => {
+//         response.json().then((data) => {
+//             if (data.error) {
+//                 messageOne.textContent = data.error
+//             } else {
+//                 messageOne.textContent = data[0].name
+//                 messageTwo.textContent = data[0].ip_address
+//                 messageThree.textContent = data[0].mac_address
+//                 messageFour.textContent = data[0].gateway_ip
+//                 messageFive.textContent = data[0].type
+
+//             }
+//         })
+
+//     })
+
+// });
+
+// buttonkayit.addEventListener('click', function(e) {
+//     e.preventDefault()
+
+//     messageSave.textContent = 'Information Saved!'
+    
+//     fetch('/save').then((response) => {
+//     })
+// })
+
+eth0Form.addEventListener('click', function(e) {
     e.preventDefault()
+    IP_Address = eth0IP.value
+    Net_mask = eth0Netmask.value
+    Gateway = eth0Gateway.value
+    DNS = eth0DNS.value
+    messageEth0Save.textContent = ''
 
-
-
-    messageOne.textContent = 'Loading...'
-    messageTwo.textContent = ''
-    messageThree.textContent = ''
-    messageFour.textContent = ''
-    messageFive.textContent = ''
-
-    fetch('/interface?int=enp0s3').then((response) => {
+    fetch('/eth0save?ip=' + IP_Address + '&netmask=' + Net_mask + '&gateway=' + Gateway + '&dns=' + DNS).then((response) => {
         response.json().then((data) => {
             if (data.error) {
-                messageOne.textContent = data.error
+                messageEth0Save.textContent = data.error
             } else {
-                messageOne.textContent = data[0].name
-                messageTwo.textContent = data[0].ip_address
-                messageThree.textContent = data[0].mac_address
-                messageFour.textContent = data[0].gateway_ip
-                messageFive.textContent = data[0].type
-
+                messageEth0Save.textContent = 'Information saved to file!'
             }
         })
-
-    })
-
-});
-
-buttonkayit.addEventListener('click', function(e) {
-    e.preventDefault()
-
-    messageSave.textContent = 'Information Saved!'
-    
-    fetch('/save').then((response) => {
     })
 })
-
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('public'));
 
 // start the express web server listening on 8080
-app.listen(8080, () => {
+app.listen(3000, () => {
   console.log('listening on 8080');
 });
 
@@ -19,36 +19,66 @@ app.get('/', (req, res) => {
 });
 
 
-app.get('/interface', (req, res) => {
+// app.get('/interface', (req, res) => {
 
 
-  network.get_interfaces_list(function(err, list) {
+//   network.get_interfaces_list(function(err, list) {
 
-      if (err) {
-          return res.send({err})
-      }
+//       if (err) {
+//           return res.send({err})
+//       }
 
-      res.send(list)
+//       res.send(list)
   
-    })
+//     })
     
-})
+// })
 
-app.get('/save', (req, res) => {
+// app.get('/save', (req, res) => {
 
-  network.get_interfaces_list(function(err, list) {
+//   network.get_interfaces_list(function(err, list) {
 
-    if (err) {
-        return res.send({err})
-    }
+//     if (err) {
+//         return res.send({err})
+//     }
 
-    fs.readFile('interfaces.txt', function (err, data) {
-      if (err) throw err;
-      if(data.includes(list[0].name)){
-       console.log('Alreadey exist')
-      } else{
-        fs.appendFileSync('interfaces.txt', list[0].name + ',' + list[0].ip_address + ',' + list[0].mac_address + ',' + list[0].gateway_ip + ',' + list[0].type + '\n')
-      }
-    });
+//     fs.readFile('interfaces.txt', function (err, data) {
+//       if (err) throw err;
+//       if(data.includes(list[0].name)){
+//        console.log('Alreadey exist')
+//       } else{
+//         fs.appendFileSync('interfaces.txt', list[0].name + ',' + list[0].ip_address + ',' + list[0].mac_address + ',' + list[0].gateway_ip + ',' + list[0].type + '\n')
+//       }
+//     });
+//   })
+// })
+
+app.get('/eth0save', (req, res) => {
+  if (!req.query.ip) {
+    return res.send({
+      error: 'You must provide IP address!'
+    })
+  } else if(!req.query.netmask) {
+    return res.send({
+      error: 'You must provide Netmask!'
+    })
+  } else if(!req.query.gateway) {
+    return res.send({
+      error: 'You must provide Gateway!'
+    })
+  } else if(!req.query.dns) {
+    return res.send({
+      error: 'You must provide DNS IP!'
+    })
+  }
+
+
+  fs.readFile('interfaces.txt', function (err, data) {
+    if (err) throw err;
+
+    fs.appendFileSync('interfaces.txt', 'Eth0' + ',' + req.query.ip + ',' + req.query.netmask + ',' + 'mac_address' + ',' + req.query.gateway + ',' + req.query.dns + ',' + 'Wired' + '\n')
+  })
+  res.send({
+    IP: req.query.ip
   })
 })
